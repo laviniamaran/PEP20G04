b77a84f0b4c67be638a6bf5fb111f81140e6050e
Lavinia Maran <71395791+laviniamaran@users.noreply.github.com>
2020-11-05 17:43:45 +0200

    Update homework6.py

--- module6/homework6.py
+++ module6/homework6.py
@@ -32,3 +32,43 @@ Iterating the object will return the lot numbers produced that day
    b) Add document all classes 5P
    c) Add document all methods 5P
 """
+import time
+
+class X():
+    ''' ciresica are mere'''
+    day=time.time()
+    seri = []
+    lot = []
+    def __init__(self, start: int, nr: int ):
+        ''' ciresel vine si cere'''
+        for i in range(start,start+nr+1):
+            self.seri.append(i)
+        for i in range(start//20, (start+nr)//20+1):
+            self.lot.append(i)
+    def __iter__(self):
+        return XIter(self.lot)
+    def left(self):
+        if n%2 == 0 :
+            return "left side driving cars"
+
+    def right(self):
+        if n%2 != 0 :
+            return "right side driving cars"
+
+class XIter():
+    def __init__(self,loturi):
+        self.loturi = loturi
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.loturi:
+            return self.loturi.pop(0)
+        raise StopIteration
+x = X(101, 50)
+print(x.seri)
+print(x.lot)
+for i in x :
+    print(i)
+
+for i in range(101, 50):
+    print(next(x), end=",")
